a3e7581d2314c4a2ae87bd088394d93faaa1c5ed
Ratatouille <307338947@qq.com>
2019-01-15 22:55:42 +0800

    多进程共享全局变量--20190115 添加了互锁的练习--20190115 加入了多任务UDP聊天室的练习--20190115

--- 多任务UDP聊天器.py
+++ 多任务UDP聊天器.py
@@ -1,64 +1,45 @@
-import time
+# import time
 import threading
 import socket
 
-ip_addr = ""
-port = 0
 
-udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-tgt_addr = ("", 30000)
-udp_socket.bind(tgt_addr)
-mutex = threading.Lock()
-
-
-def receive_msg():
-
-    mutex.acquire()
-
-    receive_data = udp_socket.recvfrom(1024)
-    content, addr = receive_data
-    print("收到：%s\n 来自： %s\n" % (content.decode("gbk"), addr))
-
-    mutex.release()
+def receive_msg(udp_socket):
+    while True:
+        receive_data = udp_socket.recvfrom(1024)
+        content, addr = receive_data
+        print("收到：%s\n 来自： %s\n" % (content.decode("gbk"), addr))
 
 
-def send_msg():
-
-    global ip_addr
-    global port
-
-    mutex.acquire()
-
-    if not ip_addr:
-        ip_addr = input("请输入对方的IP： ")
-    if not port:
-        port = int(input("请输入端口号： "))
-
-    content = input("请输入要发送的内容： ")
-    udp_socket.sendto(content.encode("gbk"), (ip_addr, port))
-    print("发送成功")
-
-    mutex.release()
-
-    return content
-
-
-
+def send_msg(udp_socket, ip_addr, port):
+    while True:
+        content = input("请输入要发送的内容： ")
+        udp_socket.sendto(content.encode("gbk"), (ip_addr, port))
+        print("发送成功")
 
 
 def main():
-    while True:
-        t1 = threading.Thread(target=send_msg)
-        t2 = threading.Thread(target=receive_msg)
 
-        t1.start()
-        t2.start()
+    # 创建套接字
+    udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-        if send_msg() == "exit":
-            break
+    # 绑定本地信息
+    tgt_addr = ("", 30000)
+    udp_socket.bind(tgt_addr)
 
-    udp_socket.close()
-    print("已关闭对话")
+    # 获得对方IP和端口
+    ip_addr = input("请输入对方的IP： ")
+    port = int(input("请输入端口号： "))
+
+    # 创建进程
+    t1 = threading.Thread(target=send_msg, args=(udp_socket, ip_addr, port))
+    t2 = threading.Thread(target=receive_msg, args=(udp_socket, ))
+
+    # 启动进程
+    t1.start()
+    t2.start()
+
+    # udp_socket.close()
+    # print("已关闭对话")
 
 
 if __name__ == "__main__":
